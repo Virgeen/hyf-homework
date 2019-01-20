4ce03dbb1647e113c4a341ee14f5dbc373c69524
Virgeen <44747304+Virgeen@users.noreply.github.com>
2019-01-21 00:27:37 +0100

    Add files via upload

---
+++ javascript/javascript1/week2/week2 .js/week2.js
@@ -0,0 +1,34 @@
+// create a function that returns an empty object.
+let emptyObject= [];
+function emptyOne(){
+    return {};
+}
+
+
+// Creat a variable, timeObject and creat a function and add key (12)
+let  timeObject = ("time:");
+function object (){
+   
+}
+console.log("time:"+  12);
+
+
+// Creat array that have three  empty object
+let emptyArray = [];
+function threeEmptyObject(empty1, empty2, empty3){
+    console.log(" "+empty1+ " " +empty2+ " "+empty3);
+}
+
+
+
+//Call a function, has variable colorObject
+ 
+function threeColor(color:){
+   return threeColor;
+   
+}
+console.log(Pink, Black, Blue);
+
+
+//Meeting friend at Tuesday  arrayObject =[ {color: 'Blue'}, {color:'Black'},{color:'Pink'}];
+   
